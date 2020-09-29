fb1b8f2c9c6877d2481efed44f818a0df06132ba
HectorPalacio <palaciorivas@gmail.com>
2020-09-29 11:33:16 -0500

    Emergency Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:disenos_intermedio/src/pages/pinterest_page.dart';
+import 'package:disenos_intermedio/src/pages/emergency_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños app',
-      home: PinterestPage(),
+      home: EmergencyPage(),
     );
   }
 }
